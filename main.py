634ba6fe80222c94682aae0b4bdd6c42155c9e59
nutmeg5506 <lopezfabila@hotmail.com>
2025-08-05 12:03:01 -0700

    final code

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from flask import Flask, request, jsonify, render_template, redirect
 from google.cloud import datastore
+from io import BytesIO
+import base64
+import matplotlib.pyplot as plt
 
 app = Flask(__name__)
 client = datastore.Client()
@@ -11,6 +14,10 @@ def index():
 @app.route('/form')
 def form():
     return render_template('form.html')
+
+@app.route('/analyze')
+def analyze():
+    return render_template('analyze.html')
 
 @app.route('/expenses')
 def expenses():
@@ -45,5 +52,56 @@ def delete_expense(expense_id):
     client.delete(key)
     return redirect('/expenses')
 
+@app.route('/analyze/date/<string:date_analyzed>', methods=['POST'])
+def analyze_by_date(date_analyzed):
+    query = client.query(kind='Expense')
+    results = list(query.fetch())
+
+    expenses_list = []
+    for item in results:
+        expense = dict(item)
+        expense['id'] = item.key.id_or_name
+        expenses_list.append(expense)
+        
+    expenses_list = sorted(expenses_list, key=lambda x: x['date'])
+    dates = {}
+    if date_analyzed == 'Day':
+        for expense in expenses_list:
+            if expense['date'] in dates:
+              dates[expense['date']] += expense["amount"]
+            else:
+                dates[expense['date']] = expense['amount']
+
+    elif date_analyzed == 'Month':
+        for expense in expenses_list:
+            month = expense['date'][0:7]
+            if month in dates:
+                dates[month] += expense['amount']
+            else:
+                dates[month] = expense['amount']
+
+    elif date_analyzed == 'Year':
+        for expense in expenses_list:
+            year = expense['date'][0:4]
+            if year in dates:
+                dates[year] += expense['amount']
+            else:
+                dates[year] = expense['amount']
+
+    plt.figure(figsize=(10, 5))
+    plt.plot(dates.keys(), dates.values(), marker='o')
+    plt.title(f"Expenses by {date_analyzed}")
+    plt.ylabel("Amount")
+    plt.xlabel(f"{date_analyzed}")
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+
+    img = BytesIO()
+    plt.savefig(img, format='png')
+    img.seek(0)
+    plot_url = base64.b64encode(img.getvalue()).decode()
+    
+    return render_template('analyze_result.html', plot_url=plot_url)
+
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080)
+    app.run(host='127.0.0.1', port=8080, debug=True)
