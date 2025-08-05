c79e631e74c370fcd05eaf5867232e3ef503c532
nutmeg5506 <lopezfabila@hotmail.com>
2025-08-05 12:28:59 -0700

    added comments

--- main.py
+++ main.py
@@ -19,41 +19,52 @@ def form():
 def analyze():
     return render_template('analyze.html')
 
+#shows the saved expenses ordered by date
 @app.route('/expenses')
 def expenses():
+    #retrieves the saved expenses from Datastore
     query = client.query(kind='Expense')
     results = list(query.fetch())
 
     expenses_list = []
+
+    #converts the JSON fetched from the database into a dictionary, adds the element id and appends to an expenses list
     for item in results:
         expense = dict(item)
         expense['id'] = item.key.id_or_name
         expenses_list.append(expense)
-        
+
+    #sorts the list by date    
     expenses_list = sorted(expenses_list, key=lambda x: x['date'])
     return render_template('expenses.html', expenses=expenses_list)
 
+#adds more expenses to the database
 @app.route('/add_expense', methods=['POST'])
 def add_expense():
     data = request.get_json()
 
+    #checks if there was data submitted through the form.html, returns an error if there is something missing
     if not data or not all(k in data for k in ('date', 'amount', 'category')):
         return jsonify({"error": "Missing required fields"}), 400
 
+    #creates a datastore entity with the new data and saves it into the database
     expense = datastore.Entity(key=client.key('Expense'))
     expense.update(data)
     client.put(expense)
 
     return jsonify({"status": "saved", "data": data}), 201
 
+#deletes the specified expense by their id
 @app.route('/delete_expense/<int:expense_id>', methods=['POST'])
 def delete_expense(expense_id):
     key = client.key('Expense', expense_id)
     client.delete(key)
     return redirect('/expenses')
 
+#visualizes the data with a line graph. It can be by day, month or year
 @app.route('/analyze/date/<string:date_analyzed>', methods=['POST'])
 def analyze_by_date(date_analyzed):
+    #retrieves the data just like in the expenses function
     query = client.query(kind='Expense')
     results = list(query.fetch())
 
@@ -64,17 +75,20 @@ def analyze_by_date(date_analyzed):
         expenses_list.append(expense)
         
     expenses_list = sorted(expenses_list, key=lambda x: x['date'])
-    dates = {}
+
+    dates = {} #dictionary to store the total amount by dates
+
+    #checks if the user wants to analyze the data by day, month, or year
     if date_analyzed == 'Day':
         for expense in expenses_list:
             if expense['date'] in dates:
-              dates[expense['date']] += expense["amount"]
+              dates[expense['date']] += expense["amount"] #adds the amount to the total for that date
             else:
-                dates[expense['date']] = expense['amount']
+                dates[expense['date']] = expense['amount'] #initializes the dictionary with the date as the key and the first amount seen
 
     elif date_analyzed == 'Month':
         for expense in expenses_list:
-            month = expense['date'][0:7]
+            month = expense['date'][0:7] #slices the stored date so that it only shows year and month. Stored dates are in the form YYYY-MM-DD. Used to analyze only the months
             if month in dates:
                 dates[month] += expense['amount']
             else:
@@ -82,12 +96,13 @@ def analyze_by_date(date_analyzed):
 
     elif date_analyzed == 'Year':
         for expense in expenses_list:
-            year = expense['date'][0:4]
+            year = expense['date'][0:4] #slices the stored date so that it only shows the year. Used to analyze only the years
             if year in dates:
                 dates[year] += expense['amount']
             else:
                 dates[year] = expense['amount']
 
+    #plots the data in a line graph
     plt.figure(figsize=(10, 5))
     plt.plot(dates.keys(), dates.values(), marker='o')
     plt.title(f"Expenses by {date_analyzed}")
@@ -96,6 +111,7 @@ def analyze_by_date(date_analyzed):
     plt.xticks(rotation=45)
     plt.tight_layout()
 
+    #stores the graph as an image to send to the html. Saves it in-memory so that is doesn't save anything to the local disk
     img = BytesIO()
     plt.savefig(img, format='png')
     img.seek(0)
